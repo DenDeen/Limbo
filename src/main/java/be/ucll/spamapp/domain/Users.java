7f16ad3c5f136f3b1ab097c793d740be46ab9650
DenDeen <mikkel.skovdal@student.ucll.be>
2020-02-22 00:47:25 +0100

    Bugfix

--- src/main/java/be/ucll/spamapp/domain/Users.java
+++ src/main/java/be/ucll/spamapp/domain/Users.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 
 public class Users {
 
-    private ArrayList<Person> users = new ArrayList<>();
+    private ArrayList<User> users = new ArrayList<>();
 
     public Users(){
 
     }
 
-    public void setPersons(ArrayList<Person> persons) {
+    public void setPersons(ArrayList<User> persons) {
         this.users = persons;
     }
 
-    public ArrayList<Person> getPersons() {
+    public ArrayList<User> getPersons() {
         return users;
     }
 
-    public void addPerson(Person person){
+    public void addPerson(User person){
         users.add(person);
     }
 
-    public Person removeNextPerson(Person person){
+    public User removeNextPerson(User person){
         return users.remove(0);
     }
 
