a0fa3fbaf1af3d83876ee6b6138de9b6acaa3af1
Epic Martijn <martijn.meeldijk@student.ucll.be>
2020-02-22 09:49:50 +0100

    profile fix

--- src/main/java/be/ucll/spamapp/controller/ProfileController.java
+++ src/main/java/be/ucll/spamapp/controller/ProfileController.java
@@ -7,14 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @ComponentScan
 @Controller
 public class ProfileController {
     ProfileController(){}
 
-    @GetMapping("/profile/?name={email}")
-    public String load(Model model, @PathVariable String email) {
+    @GetMapping("/profile")
+    public String load(Model model, @RequestParam("user") String email) {
         User user = Facade.getUser(email);
         System.out.println(user.getEmail());
 
