f01fba5d5da53ca3d6125b91d3d60ef784f447b6
DenDeen <mikkel.skovdal@student.ucll.be>
2020-02-22 02:01:13 +0100

    help

--- src/main/java/be/ucll/spamapp/controller/IndexController.java
+++ src/main/java/be/ucll/spamapp/controller/IndexController.java
@@ -1,28 +1,35 @@
 package be.ucll.spamapp.controller;
 
-import be.ucll.spamapp.domain.Restaurant;
-import be.ucll.spamapp.domain.Restaurants;
-import be.ucll.spamapp.domain.Spam;
+import be.ucll.spamapp.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @ComponentScan
 @Controller
 public class IndexController {
 
-    @Autowired
-    private Restaurants restaurants;
+    //@Autowired
+    //private User user;
     public IndexController(){
 
     }
 
-    @GetMapping("/")
+    @GetMapping("/login")
     public String printRestaurants(Model model) {
-        model.addAttribute("restaurants", restaurants.getRestaurants());
-        return "index";
+        //model.addAttribute("user", user.getAge());
+        return "";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(@RequestParam("email") String email, @RequestParam("password") String password){
+        System.out.println(email);
+        return "";
     }
 }
 
